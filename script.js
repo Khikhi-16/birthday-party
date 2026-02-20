c76a53766bc5742dbf04cf1474e049e4f2c7e373
Khikhi-16 <minnieee16kp@gmail.com>
2026-02-20 20:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let guestName = "";
+let guestAvatar = "";
 
+function switchScreen(current, next) {
+  document.getElementById(current).classList.add("hidden");
+  document.getElementById(next).classList.remove("hidden");
+}
+
+function goToAvatar() {
+  guestName = document.getElementById("guestName").value;
+  if (guestName.trim() === "") {
+    alert("Enter your name 😌");
+    return;
+  }
+  switchScreen("nameScreen", "avatarScreen");
+}
+
+function selectAvatar(type) {
+  guestAvatar = type;
+  switchScreen("avatarScreen", "doorScreen");
+}
+
+function enterHall() {
+  document.getElementById("welcomeText").innerText =
+    "Welcome " + guestName + " 🎉";
+  switchScreen("doorScreen", "hallScreen");
+}
+
+function goToThankYou() {
+  switchScreen("hallScreen", "thankYouScreen");
+}
